f53b4e0de5115c545ffce2d3d3e4977ae510fbb8
SanchezAlex <malinovskiy.alexandr@gmail.com>
2016-12-11 21:46:13 +0200

    variants catalog - fixed position

---
+++ src/js/advicePagination.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#compact-pagination').pagination({
+        items: 8,
+        itemsOnPage: 4,
+        displayedPages: 2,
+        currentPage: 1,
+        selectOnClick: true
+    });
+});
