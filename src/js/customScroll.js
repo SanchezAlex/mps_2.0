cd5c02e9e9daa3f760d096c5e5e3d4a857ce77d0
SanchezAlex <malinovskiy.alexandr@gmail.com>
2016-12-11 14:20:28 +0200

    footer - added custom scroll

---
+++ src/js/customScroll.js
@@ -0,0 +1,8 @@
+(function($){
+    $(window).on("load",function() {
+        $(".footer__countries--box").mCustomScrollbar({
+            theme: "rounded-dots",
+            scrollInertia: 200
+        });
+    });
+})(jQuery);
